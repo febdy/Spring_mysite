7a99b2181cdfdc1ef1c88f44c2e21dbec03095e8
febdy <feb29dy@gmail.com>
2018-02-12 09:36:55 +0900

    Transaction. - 작업이 모두 성공했을 때만 DB에 반영한다.

--- src/main/java/com/javaex/service/BoardService.java
+++ src/main/java/com/javaex/service/BoardService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.javaex.dao.BoardDao;
 import com.javaex.vo.BoardVo;
@@ -37,6 +38,7 @@ public class BoardService {
 		return boardDao.getArticle(no);
 	}
 
+	@Transactional
 	public BoardVo view(int no) {
 		BoardVo boardVo = getArticle(no);
 		boardDao.updateHit(no);
